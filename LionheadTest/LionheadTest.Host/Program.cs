dc9f1468202e66d83e44b3998ebc8f9642d8dee7
Ben Gale <ben@bengale.io>
2015-07-14 17:50:37 +0100

    Initial Project Structure
    Added the web project with a test controller so that I can make sure I've wired everything up correctly. Self-hosted from a console application that can be deployed as a service.

---
+++ LionheadTest/LionheadTest.Host/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using LionheadTest.API;
+using Microsoft.Owin.Hosting;
+
+namespace LionheadTest.Host
+{
+    class Program
+    {
+        static void Main()
+        {
+            const string urlBase = "http://localhost:9000";
+
+            WebApp.Start<Startup>(url: urlBase);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LionheadTest/src/LionheadTest.API/Startup.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+using Owin;
+
+namespace LionheadTest.API
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var configuration = new HttpConfiguration();
+            configuration.MapHttpAttributeRoutes();
+
+            appBuilder.UseWebApi(configuration);
+        }
+    }
+}
